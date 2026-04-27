bf8a50db10e7b7cdc0384f50b06ec803b47ea5be
mikeycab3 <miguelcabreramunoz287@gmail.com>
2026-04-27 10:36:34 -0600

    Se agrega ladingPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
 import { LoginComponent } from './core/auth/login/login.component';
+import { LandingPageComponent } from './shared/landing-page/landing-page.component';
 
 export const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'employees', component: EmployeeListComponent},
   {path:'employees-add', component:EmployeeAddComponent},
   {path:'edit-employee/:id', component:EmployeeEditComponent},
-  {path:'', redirectTo:'employees', pathMatch:'full'}
+  {path:'landing', component:LandingPageComponent},
+  {path:'', redirectTo:'landing', pathMatch:'full'}
 ];
